f12ece8b451f289d754816615adba006d9c90ac1
okamoto <yohei-okamoto@astecpaints.co.jp>
2025-01-18 11:25:38 +0900

    モックデータ検索

--- components/mockSkills.js
+++
@@ -1,9 +0,0 @@
-// components/mockSkills.js
-const mockSkills = [
-  { id: 1, name: "Web Development", icon: "🌐" },
-  { id: 2, name: "Graphic Design", icon: "🎨" },
-  { id: 3, name: "Digital Marketing", icon: "📈" },
-  { id: 4, name: "Translation", icon: "🈂️" }
-];
-
-export default mockSkills;
